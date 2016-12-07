a20ac994fc3171144ebd327be892917e848ff40a
wonjin <wjshin39@gmail.com>
2016-12-07 16:16:46 +0900

    line 삭제

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "lib/diff-match-patch-cpp-stl/diff_match_patch.h"
-2 #include <string>
-3 #include <iostream>
-4
+#include <string>
+#include <iostream>
 
-5 using namespace std;
-6 int main(int argc, char **argv) {
-7   diff_match_patch<wstring> dmp;
-8   wstring str1 = L"First string in diff";
-9   wstring str2 = L"Second string in diff";
-10   wstring strPatch = dmp.patch_toText(dmp.patch_make(str1, str2));
-11   pair<wstring, vector<bool> > out
-12     = dmp.patch_apply(dmp.patch_fromText(strPatch), str1);
-13   wstring strResult = out.first;
-14   // here, strResult will equal str2 above.
-15   wcout << strResult << endl;
-16   return 0;
-17 }
+using namespace std;
+int main(int argc, char **argv) {
+ diff_match_patch<wstring> dmp;
+ wstring str1 = L"First string in diff";
+ wstring str2 = L"Second string in diff";
+ wstring strPatch = dmp.patch_toText(dmp.patch_make(str1, str2));
+ pair<wstring, vector<bool> > out
+   = dmp.patch_apply(dmp.patch_fromText(strPatch), str1);
+ wstring strResult = out.first;
+ // here, strResult will equal str2 above.
+ wcout << strResult << endl;
+ return 0;
+}
